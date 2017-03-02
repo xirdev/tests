1389c6a411a2ef1f7046f8330c2fce24fd18794d
Ritchie Turner <ritchie@async.cl>
2017-03-02 16:48:48 -0300

    update

--- test/turn.js
+++ test/turn.js
@@ -2,10 +2,10 @@ var assert = require('chai').assert;
 var _ = require("lodash")
 var base = require('./base')
 
-describe('turn', function () {
-    describe("create ice string", function () {
-        it('should return {ok,token}', function (done) {
-            base.put("_turn/", null, function (res, curl) {
+describe('turn', function() {
+    describe("create ice string", function() {
+        it('should return {ok,token}', function(done) {
+            base.put("_turn/", null, function(res, curl) {
                 assert(res.s == "ok", "instead returns " + JSON.stringify(res))
                 assert.isObject(res.v, "is not a string" + JSON.stringify(res))
                 done()
